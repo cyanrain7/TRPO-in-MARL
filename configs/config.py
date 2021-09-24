0cf800e97861629392ba4b26c065804579f5c8d6
cyanrain7 <chenrq@shanghaitech.edu.cn>
2021-09-24 11:35:36 +0800

    fix bug

--- configs/config.py
+++ configs/config.py
@@ -246,7 +246,7 @@ def get_config():
     parser.add_argument("--use_clipped_value_loss", action='store_false', 
                         default=True, help="by default, clip loss value. If set, do not clip loss value.")
     parser.add_argument("--clip_param", type=float, 
-                        default=0.3, help='ppo clip parameter (default: 0.2)')
+                        default=0.2, help='ppo clip parameter (default: 0.2)')
     parser.add_argument("--num_mini_batch", type=int, 
                         default=1, help='number of batches for ppo (default: 1)')
     parser.add_argument("--entropy_coef", type=float, 
